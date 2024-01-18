74eababd2b782bc191cac072951175b062092acf
cayman444 <artursavinX@yandex.ru>
2024-01-18 14:30:55 +0300

    feat: add solution first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const readStream = fs.createReadStream(path.join(__dirname, './text.txt'));
+
+readStream.on('data', (chunk) => {
+  console.log(chunk.toString());
+});
+
+readStream.on('error', (error) => console.log('Error', error.message));
